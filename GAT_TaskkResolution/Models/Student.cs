c6d1288b331535845fc38b8a385802c241b9f5e3
Bilal Akhtar <bilal_hitman@hotmail.com>
2022-11-28 04:07:45 +0400

    initial push

---
+++ GAT_TaskkResolution/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GAT_TaskkResolution.Models
+{
+    public class Student
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Number { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        public string MidName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        IQueryable<Subject> Subjects { get; set; }
+    }
+}
---
+++ GAT_TaskkResolution/Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GAT_TaskkResolution.Models
+{
+    public class Subject
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Code { get; set; }
+    }
+}
